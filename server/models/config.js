c781d5e58a690a40b37013ba43b823ba3e7a580a
Uddhav-Saikia <154363974+Uddhav-Saikia@users.noreply.github.com>
2025-08-07 23:59:08 +0530

    Add models for ClubAdmin, ClubPost, ClubUser, UserDetail,

---
+++ server/models/config.js
@@ -0,0 +1,7 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("Database connected successfully");
+});
